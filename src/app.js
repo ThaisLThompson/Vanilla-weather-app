aaba7ca7abaae9ef509d132b3e5b7d195bff1d36
ThaisLThompson <104031080+ThaisLThompson@users.noreply.github.com>
2022-07-28 21:01:47 -0400

    change time format to 12hr format

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
-  let hours = date.getHours();
+  let currentHours = date.getHours();
+  let hours = currentHours % 12 || 12;
   if (hours < 10) {
     hours = `0${hours}`;
   }
@@ -66,7 +67,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "b058b0bdb3b4b3e3de7668710049cf5a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
